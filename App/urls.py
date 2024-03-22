d3a6bbfda7fbc36852eeba786485e94b237e4a0b
Uzair <uzairarifjokhio@gmail.com>
2024-03-22 22:57:49 +0500

    changed the urls and added blogs

--- App/urls.py
+++ App/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     # path('/home', views.home, name='home'),
     path('', views.product_list, name='product_list'),
     path('contact/', views.contact_page, name='contact_page'),
-    path('<int:pk>/', views.product_detail, name='product_detail'),
-    path('<int:pk>/edit/', views.edit_product, name='edit_product'),
-    path('<int:pk>/delete/', views.delete_product, name='delete_product'),
+    path('blog/<int:pk>/', views.product_detail, name='product_detail'),
+    path('blog/<int:pk>/edit/', views.edit_product, name='edit_product'),
+    path('blog/<int:pk>/delete/', views.delete_product, name='delete_product'),
 ]
