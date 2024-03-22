8aed2d001fe992a22e7555411f7d431bb30bfa2e
Uzair <uzairarifjokhio@gmail.com>
2024-03-22 23:00:55 +0500

    forms added widgets in in ProductForm and ContactForm

--- App/forms.py
+++ App/forms.py
@@ -6,14 +6,20 @@ class ProductForm(forms.ModelForm):
         model = Product
         fields = ['name', 'description', "image"]
 
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 4,'style': 'resize: none;'}),
+            'image': forms.ClearableFileInput(attrs={'class': 'form-control-file'})
+        }
+
 class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
         fields = ['full_name', 'email', 'subject', 'message']
 
         widgets ={
-            "full_name": forms.TextInput(attrs={'class':'form-control'}),
-            'email': forms.EmailInput(attrs={'class':'form-control'}),
-            'subject': forms.TextInput(attrs={'class':'form-control'}),
-            'message': forms.Textarea(attrs={'class':'form-control'}),
+            "full_name": forms.TextInput(attrs={'class':'form-control', 'placeholder':'Enter your full Name'}),
+            'email': forms.EmailInput(attrs={'class':'form-control', 'placeholder':'Enter your email address'}),
+            'subject': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Enter the subject'}),
+            'message': forms.Textarea(attrs={'class':'form-control', 'placeholder':'Enter the message'}),
         }
