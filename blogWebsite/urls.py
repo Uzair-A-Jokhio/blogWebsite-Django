30f52b53b9318ade4e8602b8e320862d4e13ab06
Uzair <uzairarifjokhio@gmail.com>
2024-03-25 01:37:27 +0500

    setting the django.contrib.auth.urls in Blogwebsite.urls

--- blogWebsite/urls.py
+++ blogWebsite/urls.py
@@ -22,8 +22,14 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("App.urls"))
+    path('', include("App.urls")),
+    path('members/', include('django.contrib.auth.urls')),
+    path('members/', include('members.urls')),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+admin.site.site_header = "BuzzFeedle Administration Page "
+admin.site.site_title = "Browser Title"
+admin.site.index_title = "Welcome to Admin Area...  "
