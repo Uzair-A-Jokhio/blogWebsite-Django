370a2d506ae2c9aed119ce0de7e72d7587011545
Uzair <uzairarifjokhio@gmail.com>
2024-03-27 22:47:22 +0500

    Signup system added

--- members/views.py
+++ members/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import UserSignupForm
+
+from django.core.mail import send_mail
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import get_template
 
 # Create your views here.
 
@@ -24,3 +29,24 @@ def logout_user(request):
     logout(request)
     messages.info(request, "You have been Logged OUT")
     return redirect('product_list')
+
+def signup_user(request):
+    if request.method == "POST":
+        form = UserSignupForm(request.POST)
+        if form.is_valid:
+            form.save()   
+            messages.success(request, "Your account has been created! Now you can Login")
+            return redirect('login')
+    else:
+        form = UserSignupForm()
+    return render(request, 'authen/signup.html', {'form':form})
+
+
+def welcome_mail(username, email):
+    htmly = get_template('authen/email.html')
+    d = { 'username': username }
+    subject, from_email, to = 'welcome', 'your_email@gmail.com', email
+    html_content = htmly.render(d)
+    msg = EmailMultiAlternatives(subject, html_content, from_email, [to])
+    msg.attach_alternative(html_content, "text/html")
+    msg.send()
